c810a69dee23660f51aac4b31f406d0069b4da5f
ychen514 <ychen514@uic.edu>
2024-04-22 10:47:31 -0800

    update

--- llama_server_hf_mocker_1.py
+++ llama_server_hf_mocker_1.py
@@ -178,7 +178,7 @@ def task1_data_receiving(args, inputs):
     print(f'{pid} with thread {curr_thread}, with process: {curr_process} Started')
     print('T1 do nothing!')
     for i in range(0, 5):
-        sleep(0.002)
+        sleep(0.01)
         incoming_queue.put(inputs[i])
 
 def task2_computation(models, start_idx, end_idx, tokenizer, device, is_dummy=True):
