78de7ffc9d9f1966ee04eb6679ede401e228b387
ychen514 <ychen514@uic.edu>
2024-05-12 01:21:39 -0800

    update

--- http_receiver.py
+++ http_receiver.py
@@ -65,14 +65,14 @@ class S(BaseHTTPRequestHandler):
         #incoming_queue.append(decrypt_data)
         end_time = time.time()
         print('server receiving time: ', end_time - start_time)
-        '''# Process the received data here:
+        # Process the received data here:
         self.send_response(200)
         self.end_headers()
 
         newx = pickle.dumps('Data received successfully!')
-        self.wfile.write(newx)'''
+        self.wfile.write(newx)
 
-        self.return_message()
+        #self.return_message()
 
     def return_message(self):
         '''outgoing_data = []
