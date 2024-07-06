ba0897daa39e8f3d29c6a1224822aba158a9d019
ychen514 <ychen514@uic.edu>
2024-07-06 15:19:57 -0800

    update

--- eval.py
+++ eval.py
@@ -258,7 +258,7 @@ def eval_ppl_wikitext_sep_hf(models, testenc, tokenizer, splitting_point, bs=1, 
         for k in range (1, len(models) - 2):
             start_time = time.time()
             out, ids, mask = models[k](out.last_hidden_state, position_ids=ids, attention_mask=mask)
-            if k == splitting_point:
+            '''if k == splitting_point:
                 break
                 out, ids, mask, pruned_data_idx_list, pruned_data_list = early_exit_cuda_ppl_test(models, out, ids, mask)
 
@@ -276,7 +276,7 @@ def eval_ppl_wikitext_sep_hf(models, testenc, tokenizer, splitting_point, bs=1, 
                         #zeros_row = torch.zeros((1, 1, 1, mask.size(3))).to(device)
                         #mask = torch.cat((mask[:, :, :l, :], zeros_row, mask[:, :, l:, :]), dim=2)
 
-            end_time = time.time()
+            end_time = time.time()'''
             #print(k, end_time - start_time)
             #print('out: ', out)
 
