1c4b6e6ed08961f649764befae7320617a1990b7
ychen514 <ychen514@uic.edu>
2024-07-08 06:42:02 -0800

    update

--- llama_edge_hf_early_exit_jetson.py
+++ llama_edge_hf_early_exit_jetson.py
@@ -274,7 +274,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
             print('waiting...')
             time.sleep(1)
 
-        if calculate_opt.incoming_count >= calculate_opt.outgoint_count and outgoing_queue.qsize() < 3:
+        if calculate_opt.incoming_count >= calculate_opt.outgoint_count and outgoing_queue.qsize() < 2:
             outgoing_queue.put([1, out, ids, mask])
             end_time = time.time()
             print('client computation time: ', end_time - start_time)
