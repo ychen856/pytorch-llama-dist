c5cedce224c2d0c51715467111fda4cb3be16d0c
ychen514 <ychen514@uic.edu>
2024-07-08 04:38:55 -0800

    update

--- llama_edge_hf_early_exit_jetson.py
+++ llama_edge_hf_early_exit_jetson.py
@@ -371,8 +371,7 @@ if __name__ == '__main__':
     head_idx = 2
 
     models = load_model(args.ckpt_dir_hf_sep, start_idx, end_idx_buff, device)
-    #lm_models = load_lm_head(args.ckpt_dir_hf_sep, head_idx, device, cache_dir="llm_weights")
-    lm_models = None
+    lm_models = load_lm_head(args.ckpt_dir_hf_sep, head_idx, device, cache_dir="llm_weights")
     tokenizer = LlamaTokenizer.from_pretrained(args.ckpt_dir_hf, use_fast=False)
 
 
