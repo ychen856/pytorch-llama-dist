a8ea84416a3c1243c414a873ae9087df9616028a
ychen514 <ychen514@uic.edu>
2024-07-08 06:35:59 -0800

    update

--- llama_edge_hf_early_exit_jetson.py
+++ llama_edge_hf_early_exit_jetson.py
@@ -339,7 +339,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
         #if end_idx_buff < end_idx and end_idx_buff + 3 <= max_layers:  #add buffer
         if end_idx_buff < end_idx and end_idx_buff < max_layers:
             models, end_idx_buff = layer_reallocation(1, start_idx, end_idx_buff, max_layers, models)
-        while end_idx_buff > end_idx + 1:  #remove buffer
+        while end_idx_buff > end_idx + 3:  #remove buffer
             models, end_idx_buff = layer_reallocation(2, start_idx, end_idx_buff, max_layers, models)
         gc.collect()
         torch.cuda.empty_cache()
