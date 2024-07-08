6f2f6cf7b0f86f3c8252e983f8191c53e1c6582d
ychen514 <ychen514@uic.edu>
2024-07-08 03:11:31 -0800

    update

--- llama_edge_hf_early_exit_jetson.py
+++ llama_edge_hf_early_exit_jetson.py
@@ -275,11 +275,11 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
         for k in range(1, end_idx + 1):
             try:
                 out, ids, mask = models[k](out.last_hidden_state, position_ids=ids, attention_mask=mask)
-                if k == head_idx:
+                '''if k == head_idx:
                     is_early_exit, lm_logits = early_exit_lm_head(lm_models, out)
                     print('is early: ', is_early_exit)
                     if is_early_exit:
-                        break
+                        break'''
             except Exception as e:
                 print('oom!!!')
                 is_oom = True
@@ -366,7 +366,8 @@ if __name__ == '__main__':
     head_idx = 4
 
     models = load_model(args.ckpt_dir_hf_sep, start_idx, end_idx_buff, device)
-    lm_models = load_lm_head(args.ckpt_dir_hf_sep, head_idx, device, cache_dir="llm_weights")
+    #lm_models = load_lm_head(args.ckpt_dir_hf_sep, head_idx, device, cache_dir="llm_weights")
+    lm_models = None
     tokenizer = LlamaTokenizer.from_pretrained(args.ckpt_dir_hf, use_fast=False)
 
 
