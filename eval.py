7d17c988dde3b03541ab5bfa13e99d4862f15598
ychen514 <ychen514@uic.edu>
2024-07-08 17:51:29 -0800

    update

--- eval.py
+++ eval.py
@@ -368,24 +368,23 @@ def eval_lm_head_ppl_wikitext_sep_hf(models, lm_models, testenc, tokenizer, spli
         # Forward pass through the model
         out, ids, mask = models[0](inputs)
         is_early_exit = False
-        for k in range(1, len(models)):
-        #for k in range (1, len(models) - 2):
+        for k in range (1, len(models) - 2):
             is_early_exit = False
             #print('Processing layer: ', k)
             start_time = time.time()
             out, ids, mask = models[k](out.last_hidden_state, position_ids=ids, attention_mask=mask)
             #print('mask: ', mask)
-            if k == splitting_point:
+            '''if k == splitting_point:
                 is_early_exit, lm_logits = early_exit_lm_head(lm_models, out)
-                '''if is_early_exit:
+                if is_early_exit:
                     is_early_exit = True
                     early_count = early_count + 1
                     print('early: ', early_count)
-                    break
+                    break'''
 
         if not is_early_exit:
             lm_logits = models[33](out.last_hidden_state)
-            lm_logits = models[34](lm_logits)'''
+            lm_logits = models[34](lm_logits)
 
 
 
