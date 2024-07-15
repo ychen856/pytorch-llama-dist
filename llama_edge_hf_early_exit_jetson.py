03756e6758dd5ca80e0d04fe8069ae137b543ca5
ychen514 <ychen514@uic.edu>
2024-07-15 00:12:20 -0800

    update

--- llama_edge_hf_early_exit_jetson.py
+++ llama_edge_hf_early_exit_jetson.py
@@ -270,9 +270,9 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
 
 
         print('outgoing queue size: ', outgoing_queue.qsize())
-        '''while outgoing_queue.qsize() > 3:
+        while outgoing_queue.qsize() > 5:
             print('waiting...')
-            time.sleep(1)'''
+            time.sleep(1)
 
         # if server idle
         if calculate_opt.incoming_count >= calculate_opt.outgoint_count and outgoing_queue.qsize() < 3:
