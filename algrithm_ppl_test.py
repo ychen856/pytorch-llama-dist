68fe4cefd84c1508bf9cc2a4947255cb8f803d47
ychen514 <ychen514@uic.edu>
2024-07-31 02:33:28 -0800

    update

--- algrithm_ppl_test.py
+++ algrithm_ppl_test.py
@@ -267,7 +267,7 @@ if __name__ == '__main__':
             out, ids, mask = models[0](inputs)
             is_early_exit = False
 
-            lm_head, lm_head_idx = get_lm_head_idx(end_idx)
+            lm_head, lm_head_idx = get_lm_head_idx(end_idx[i])
 
             # for k in range (1, len(models) - 2):
             for k in range(1, len(models)):
