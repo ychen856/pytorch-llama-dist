688dd044b47479e3edf30cdbfc5cef4b7e6be28a
ychen514 <ychen514@uic.edu>
2024-07-31 03:08:00 -0800

    update

--- algrithm_ppl_test.py
+++ algrithm_ppl_test.py
@@ -270,7 +270,7 @@ if __name__ == '__main__':
             #lm_head, lm_head_idx = get_lm_head_idx(end_idx[i])
 
             # for k in range (1, len(models) - 2):
-            for k in range(1, len(models)):
+            for k in range(1, len(models) - 2):
                 is_early_exit = False
                 # print('Processing layer: ', k)
                 start_time = time.time()
