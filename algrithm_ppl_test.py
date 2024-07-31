0825627b4b5ca21b45da58fd218d50c56337f90f
ychen514 <ychen514@uic.edu>
2024-07-31 00:55:52 -0800

    add algorithm_ppl_test.py

---
+++ algrithm_ppl_test.py
@@ -0,0 +1,345 @@
+# this code is used for seperating the weights into small pieces and store them into seperated .pt files. One time usage.
+
+from typing import Optional
+
+import numpy as np
+import torch
+import time
+from pathlib import Path
+import json
+from sentencepiece import SentencePieceProcessor
+from tqdm import tqdm
+import argparse
+from data import get_loaders
+import torch.nn as nn
+import safetensors
+
+from safetensors.torch import save_file
+from transformers import PreTrainedTokenizerFast, LlamaTokenizer, AutoModelForCausalLM, LlamaConfig, AutoConfig
+
+import sys
+
+from early_exit import early_exit_lm_head
+from eval import eval_ppl_sep_hf, eval_lm_head_ppl_sep_hf
+from eval_sep_hf import get_eval_data
+from layerwrapper import WrappedGPT
+from model_hf import LlamaForCausalLM, LlamaForCausalLM_emb, LlamaForCausalLM_layer_0, LlamaForCausalLM_norm, \
+    LlamaForCausalLM_linear
+import yaml
+from model import ModelArgs, Transformer
+from model_dist import Transformer_emb, Transformer_b0, Transformer_b1, Transformer_b2, Transformer_b3, \
+    Transformer_b4, Transformer_b5, Transformer_b6, Transformer_b7, Transformer_b8, Transformer_b9, Transformer_b10, Transformer_b11, \
+    Transformer_b12, Transformer_b13, Transformer_b14, Transformer_b15, Transformer_b16, Transformer_b17, Transformer_b18, Transformer_b19, \
+    Transformer_b20, Transformer_b21, Transformer_b22, Transformer_b23, Transformer_b24, Transformer_b25, Transformer_b26, Transformer_b27, \
+    Transformer_b28, Transformer_b29, Transformer_b30, Transformer_b31, Transformer_norm, Transformer_linear
+from prune_all import prepare_calibration_input_opt, prepare_calibration_input, find_layers, check_outlier_mean, \
+    return_given_alpha, check_sparsity
+
+parser = argparse.ArgumentParser(
+    description='Pytorch Imagenet Training')
+parser.add_argument('--config', default='config_server.yaml')
+args = parser.parse_args()
+
+
+def get_llm(model, cache_dir="llm_weights"):
+    model = AutoModelForCausalLM.from_pretrained(
+        model,
+        torch_dtype=torch.float16,
+        cache_dir=cache_dir,
+        low_cpu_mem_usage=True,
+        device_map="auto"
+    )
+
+    model.seqlen = 1024
+    return model
+
+
+def load_model(checkpoints_dir, start_idx, end_idx, device):
+    config, kwargs = AutoConfig.from_pretrained(
+        args.ckpt_dir_hf,
+        return_unused_kwargs=True
+    )
+    print('config: ', config)
+
+    checkpoint_list = []
+    checkpoints = sorted(Path(checkpoints_dir).glob("consolidated.*.pth"))
+    assert len(checkpoints) > 0, f"no checkpoint files found in {checkpoints_dir}"
+
+    checkpoint_idx = 0
+    for checkpoint in checkpoints:
+        ckpt_path = checkpoint
+        print(f'Loading checkpoint "{ckpt_path}"')
+
+        checkpoint_list.append(torch.load(ckpt_path, map_location="cpu"))
+        checkpoint_idx = checkpoint_idx + 1
+        if checkpoint_idx > end_idx:
+            break
+
+    if device.type == 'cuda':
+        torch.set_default_tensor_type(torch.cuda.HalfTensor)
+    else:
+        torch.set_default_tensor_type(torch.BFloat16Tensor)
+
+    models = []
+    for i in range(start_idx, end_idx + 1):
+        print('i: ', i)
+        if i == 0:
+            models.append(LlamaForCausalLM_emb(config))
+            models[i].load_state_dict(checkpoint_list[i], strict=True)
+            #models[0].model.embed_tokens.weight = nn.Parameter(checkpoint_list[0]['model.embed_tokens.weight'])
+            models[0].to(device)
+        elif i == 33:
+            models.append((LlamaForCausalLM_norm(config)))
+            models[i].load_state_dict(checkpoint_list[i], strict=True)
+            #models[33].model.norm.weight = nn.Parameter(checkpoint_list[33]['model.norm.weight'])
+            models[33].to(device)
+
+        elif i == 34:
+            models.append((LlamaForCausalLM_linear(config)))
+            models[i].load_state_dict(checkpoint_list[i], strict=True)
+            #models[34].lm_head.weight = nn.Parameter(checkpoint_list[34]['lm_head.weight'])
+            models[34].to(device)
+        else:
+            models.append(LlamaForCausalLM_layer_0(config))
+            models[i].load_state_dict(checkpoint_list[i], strict=True)
+
+            models[i].to(device)
+
+    '''for i in range(0, len(models)):
+        model = models[i]
+        for name, param in model.named_parameters():
+            if param.requires_grad:
+                print(name, param.data)'''
+
+    return models
+
+
+def get_lm_head_idx(end_idx):
+
+    lm_heads = [1, 2, 4]
+    lm_head = 1
+    lm_head_idx = 0
+
+    for i in range(0, len(lm_heads)):
+        if lm_heads[i] > end_idx:
+            #lm_head = lm_heads[i - 1]
+            #lm_head_idx = lm_head_idx - 1
+            break
+        elif lm_heads[i] == end_idx:
+            lm_head = lm_heads[i]
+            lm_head_idx = i
+            break
+
+        lm_head = lm_heads[i]
+        lm_head_idx = i
+
+    lm_head_idx = lm_head_idx + 1
+
+
+    return lm_head, lm_head_idx
+
+
+def load_lm_head(checkpoints_dir, end_idx, device, cache_dir="llm_weights"):
+    config, kwargs = AutoConfig.from_pretrained(
+        args.ckpt_dir_hf,
+        return_unused_kwargs=True
+    )
+    print('config: ', config)
+    print('??: ', end_idx)
+
+    lm_head, lm_head_idx = get_lm_head_idx(end_idx)
+
+    print('lm_head: ', lm_head)
+    print('lm_head_idx: ', lm_head_idx)
+
+    checkpoint_list = []
+    checkpoints = sorted(Path(checkpoints_dir).glob("lm_head.*.pth"))
+    assert len(checkpoints) > 0, f"no checkpoint files found in {checkpoints_dir}"
+
+
+    for i in range(0, len(checkpoints)):
+        if i == 0 or i == lm_head_idx:
+            ckpt_path = checkpoints[i]
+            print(f'Loading checkpoint "{ckpt_path}"')
+
+            checkpoint_list.append(torch.load(ckpt_path, map_location="cpu"))
+
+
+
+    if device.type == 'cuda':
+        torch.set_default_tensor_type(torch.cuda.HalfTensor)
+    else:
+        torch.set_default_tensor_type(torch.BFloat16Tensor)
+
+    lm_models = []
+
+    for i in range(0, len(checkpoint_list)):
+        if i == 0:
+            lm_models.append((LlamaForCausalLM_norm(config)))
+            lm_models[i].load_state_dict(checkpoint_list[i], strict=True)
+            lm_models[i].to(device)
+
+        else:
+            lm_models.append((LlamaForCausalLM_linear(config)))
+            lm_models[i].load_state_dict(checkpoint_list[i], strict=True)
+            lm_models[i].to(device)
+
+    return lm_head, lm_models
+
+
+def get_lm_head_idx(end_idx):
+
+    lm_heads = [1, 2, 4]
+    lm_head = 1
+    lm_head_idx = 0
+
+    for i in range(0, len(lm_heads)):
+        if lm_heads[i] > end_idx:
+            #lm_head = lm_heads[i - 1]
+            #lm_head_idx = lm_head_idx - 1
+            break
+        elif lm_heads[i] == end_idx:
+            lm_head = lm_heads[i]
+            lm_head_idx = i
+            break
+
+        lm_head = lm_heads[i]
+        lm_head_idx = i
+
+    lm_head_idx = lm_head_idx + 1
+
+
+    return lm_head, lm_head_idx
+
+if __name__ == '__main__':
+    with open(args.config) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    for key in config:
+        for k, v in config[key].items():
+            setattr(args, k, v)
+
+    print('config type: ', args.config)
+    torch.manual_seed(0)
+
+
+    #allow_cuda = False
+    #device = 'cuda' if torch.cuda.is_available() and allow_cuda else 'cpu'
+
+    #head_idx = 1
+
+    device = torch.device("cuda")
+    models = load_model(args.ckpt_dir_hf_sep, 0, 34, device)
+    #_, lm_models = load_lm_head(args.ckpt_dir_hf_sep, head_idx, device)
+    tokenizer = LlamaTokenizer.from_pretrained(args.ckpt_dir_hf, use_fast=False)
+
+
+
+    print("loading success")
+
+    test_loader = get_eval_data(tokenizer)
+    bs = 1
+
+    # loading inputs data
+    seqlen = 1024
+    # Get input IDs
+    testenc = test_loader.input_ids
+
+    # Calculate number of samples
+    nsamples = testenc.numel() // seqlen
+    nsamples = 10
+    # List to store negative log likelihoods
+    nlls = []
+    print(f"nsamples {nsamples}")
+
+    end_idx = [-1, 5, 6, 7, -1, 8, -1, -1, -1]
+
+    # Evaluate ppl in no grad context to avoid updating the model
+    with torch.no_grad():
+        seqlen = 1024
+        # Get input IDs
+        testenc = testenc.input_ids
+
+        # Calculate number of samples
+        nsamples = testenc.numel() // seqlen
+
+        # nsamples = 30
+        # List to store negative log likelihoods
+        nlls = []
+        print(f"nsamples {nsamples}")
+        # Loop through each batch
+
+        early_count = 0
+        for i in range(0, nsamples, bs):
+            if i % 50 == 0:
+                print(f"sample {i}")
+
+            # Calculate end index
+            j = min(i + bs, nsamples)
+
+            # Prepare inputs and move to device
+            inputs = testenc[:, (i * seqlen):(j * seqlen)].to(device)
+            inputs = inputs.reshape(j - i, seqlen)
+            # print(tokenizer.batch_decode(inputs, skip_special_tokens=True, clean_up_tokenization_spaces=False))
+
+            # Forward pass through the model
+            out, ids, mask = models[0](inputs)
+            is_early_exit = False
+
+            lm_head, lm_head_idx = get_lm_head_idx(end_idx)
+
+            # for k in range (1, len(models) - 2):
+            for k in range(1, len(models)):
+                is_early_exit = False
+                # print('Processing layer: ', k)
+                start_time = time.time()
+                out, ids, mask = models[k](out.last_hidden_state, position_ids=ids, attention_mask=mask)
+
+                if end_idx > 0:
+                    head_idx, lm_models = load_lm_head(args.ckpt_dir_hf_sep, end_idx[i], device)
+
+                # print('mask: ', mask)
+                if k == head_idx:
+                    is_early_exit, lm_logits = early_exit_lm_head(lm_models, out, head_idx)
+
+                    if is_early_exit:
+                        is_early_exit = True
+                        early_count = early_count + 1
+                        print('early: ', early_count)
+                        break
+
+            if not is_early_exit:
+                lm_logits = models[33](out.last_hidden_state)
+                lm_logits = models[34](lm_logits)
+
+            # Shift logits and labels for next token prediction
+            shift_logits = lm_logits[:, :-1, :].contiguous()
+            shift_labels = inputs[:, 1:]
+
+
+
+            # Compute loss
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(shift_logits.reshape(-1, shift_logits.size(-1)), shift_labels.reshape(-1))
+
+
+            # Calculate negative log likelihood
+            neg_log_likelihood = loss.float() * seqlen * (j - i)
+            #print('ppl: ', torch.exp(neg_log_likelihood / seqlen))
+
+            # Append to list of negative log likelihoods
+            nlls.append(neg_log_likelihood)
+
+
+            sys.stdout.flush()
+
+        print('begin calcualte ppl')
+        # Compute perplexity
+        ppl = torch.exp(torch.stack(nlls).sum() / (nsamples * seqlen))
+        # Empty CUDA cache to save memory
+        torch.cuda.empty_cache()
+        print('early count: ', early_count)
+        print('ppl: ', ppl.item())
+
+
+
+
