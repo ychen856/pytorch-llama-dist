26c614f7ddd58d7c9c5d7cee3ea795f9cfc18167
ychen514 <ychen514@uic.edu>
2024-07-31 13:18:59 -0800

    update

--- algrithm_ppl_test.py
+++ algrithm_ppl_test.py
@@ -308,6 +308,7 @@ if __name__ == '__main__':
             # Compute loss
             loss_fct = nn.CrossEntropyLoss()
             loss = loss_fct(shift_logits.reshape(-1, shift_logits.size(-1)), shift_labels.reshape(-1))
+            print('loss: ', loss)
 
 
             # Calculate negative log likelihood
