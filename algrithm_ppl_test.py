45770a44dcfbd6151f6b912c1a2375fed8f4ef83
ychen514 <ychen514@uic.edu>
2024-08-03 23:07:56 -0800

    update

--- algrithm_ppl_test.py
+++ algrithm_ppl_test.py
@@ -243,7 +243,7 @@ if __name__ == '__main__':
     nlls = []
     print(f"nsamples {nsamples}")
 
-    end_idx = [4, 4, 5, 5, 5, 6, 6, 7, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    end_idx = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 
     # Evaluate ppl in no grad context to avoid updating the model
     with torch.no_grad():
