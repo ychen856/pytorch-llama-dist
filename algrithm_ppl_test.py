a1cbffddc647ac962b92d45f4b09541af08a466e
ychen514 <ychen514@uic.edu>
2024-08-09 21:45:45 -0800

    update

--- algrithm_ppl_test.py
+++ algrithm_ppl_test.py
@@ -244,7 +244,7 @@ if __name__ == '__main__':
     print(f"nsamples {nsamples}")
 
     #end_idx = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    end_idx = [3, 3, 3, 4, 4, 5, 1, 1, 1, 1]
+    end_idx = [1, 1, 1, 1, 1, 1, 1, 2, 2, 3]
 
 
     # Evaluate ppl in no grad context to avoid updating the model
