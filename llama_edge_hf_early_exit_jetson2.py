7fd6e925521a105a699034c6705b0364eae2a221
ychen514 <ychen514@uic.edu>
2024-09-14 09:51:10 -0800

    update

--- llama_edge_hf_early_exit_jetson2.py
+++ llama_edge_hf_early_exit_jetson2.py
@@ -326,7 +326,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
     while(1):
         if input_queue.qsize() == 0:
             #time.sleep(150)
-            while len(timestamp_manager.end_times) < 30:
+            while len(timestamp_manager.end_times) < 10:
                 time.sleep(0.0001)
             timestamp_manager.get_time_diff_every_n_inputs(10)
             timestamp_manager.clearAll()
@@ -545,7 +545,7 @@ if __name__ == '__main__':
 
     # Calculate number of samples
     nsamples = testenc.numel() // seqlen
-    nsamples = 5
+    nsamples = 10
     # List to store negative log likelihoods
     nlls = []
     print(f"nsamples {nsamples}")
