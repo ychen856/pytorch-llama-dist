d074dc6f950a57dc7c5db9d867f57c85b7c69df0
ychen514 <ychen514@uic.edu>
2024-09-16 15:40:25 -0800

    update

--- llama_edge_hf_offload_everything_to_server.py
+++ llama_edge_hf_offload_everything_to_server.py
@@ -241,7 +241,7 @@ def task2_computation(models, start_idx, end_idx, end_idx_buff, max_layers, devi
 
             # Calculate number of samples
             nsamples = testenc.numel() // seqlen
-            nsamples = 5
+            nsamples = 10
             # List to store negative log likelihoods
             nlls = []
             print(f"nsamples {nsamples}")
