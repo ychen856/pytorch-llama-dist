d878e73fac6ba033e311f51b870a9526b6b84065
ychen514 <ychen514@uic.edu>
2024-09-25 01:30:37 -0800

    update

--- llama_edge_hf_early_exit_jetson2.py
+++ llama_edge_hf_early_exit_jetson2.py
@@ -140,7 +140,9 @@ def load_model(checkpoints_dir, start_idx, end_idx, device):
 
 
     if device.type == 'cuda':
-        torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        print('ZZZZZZZZZZZZZZZZZZZZ')
+        torch.set_default_dtype(torch.cuda.HalfTensor)
+        #torch.set_default_tensor_type(torch.cuda.HalfTensor)
     else:
         torch.set_default_tensor_type(torch.BFloat16Tensor)
 
