9162a1b0ed17182b50a9d8a33ea6fc6f469a23f1
ychen514 <ychen514@uic.edu>
2024-11-10 05:47:30 -0800

    update

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 #python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 1 2>&1|tee u_pc_batch_10_2_alg.log
 #python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 2 2>&1|tee u_pc_batch_10_4_alg.log
-python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 3 2>&1|tee u_pc_batch_30_2_alg.log
-python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 4 2>&1|tee u_pc_batch_30_4_alg.log
-python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 5 2>&1|tee u_jetson_batch_10_2_alg.log
-python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 6 2>&1|tee u_jetson_batch_10_4_alg.log
+#python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 3 2>&1|tee u_pc_batch_30_2_alg.log
+#python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 4 2>&1|tee u_pc_batch_30_4_alg.log
+#python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 5 2>&1|tee u_jetson_batch_10_2_alg.log
+#python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 6 2>&1|tee u_jetson_batch_10_4_alg.log
 python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 7 2>&1|tee u_jetson_batch_20_2_alg.log
 python3 algrithm_ppl_test3.py --config config_nrp.yaml --selection 8 2>&1|tee u_jetson_batch_20_4_alg.log
