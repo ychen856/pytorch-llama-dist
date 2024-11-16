937f1db0f8c1de64a127975a96f7440d5540ac3c
ychen514 <ychen514@uic.edu>
2024-11-16 12:03:14 -0800

    update

--- run_jetson.sh
+++ run_jetson.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml 2>&1|tee batch_20_1_all.log
-python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml 2>&1|tee batch_20_2_all.log
-
-python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml 2>&1|tee batch_20_3_all.log
-python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml 2>&1|tee batch_20_4_all.log
+python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml
+python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml
+python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml
+python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml
+python3.8 llama_edge_hf_early_exit_jetson2.py --config config_jetson2.yaml
