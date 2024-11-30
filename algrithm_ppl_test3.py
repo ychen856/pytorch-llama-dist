c9935da22176af48e4cd979165f76fd4b02247a0
ychen514 <ychen514@uic.edu>
2024-11-30 00:32:28 -0800

    update

--- algrithm_ppl_test3.py
+++ algrithm_ppl_test3.py
@@ -326,10 +326,10 @@ if __name__ == '__main__':
             reshaped_labels = shift_labels.view(1, -1)
 
 
-            print('text logits: ',
+            '''print('text logits: ',
                   tokenizer.batch_decode(reshaped_logit, skip_special_tokens=True, clean_up_tokenization_spaces=False))
             print('text lables: ',
-                  tokenizer.batch_decode(reshaped_labels, skip_special_tokens=True, clean_up_tokenization_spaces=False))
+                  tokenizer.batch_decode(reshaped_labels, skip_special_tokens=True, clean_up_tokenization_spaces=False))'''
 
             # Compute loss
             loss_fct = nn.CrossEntropyLoss()
