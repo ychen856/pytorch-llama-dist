8d41b992ebe338b92720f2a3feb224eea861a6bb
ychen514 <ychen514@uic.edu>
2024-12-06 01:40:46 -0800

    update

--- run.sh
+++ run.sh
@@ -9,7 +9,5 @@
 #python3 algrithm_ppl_test3.py --config config_lambda.yaml --selection 7 2>&1|tee u_jetson_batch_20_2_only.log
 #python3 algrithm_ppl_test3.py --config config_lambda.yaml --selection 8 2>&1|tee u_jetson_batch_20_4_only.log
 
-python3 lm_head_training.py --head 24 --config config_nrp.yaml
-python3 lm_head_training.py --head 26 --config config_nrp.yaml
 python3 lm_head_training.py --head 28 --config config_nrp.yaml
 python3 lm_head_training.py --head 30 --config config_nrp.yaml
