5899be5bb5b61e0e1c4afd454e21cb940478fd70
ychen514 <ychen514@uic.edu>
2024-12-25 19:24:47 -0800

    update

--- inference_lm_head.py
+++ inference_lm_head.py
@@ -14,6 +14,7 @@ from data import get_loaders
 import torch.nn as nn
 import safetensors
 import re
+from natsort import natsorted
 from safetensors.torch import save_file
 from transformers import PreTrainedTokenizerFast, LlamaTokenizer, AutoModelForCausalLM, LlamaConfig, AutoConfig
 
@@ -160,9 +161,10 @@ def load_lm_head(checkpoints_dir, end_idx, device, cache_dir="llm_weights"):
     print('lm_head_idx: ', lm_head_idx)
 
     checkpoint_list = []
-    #checkpoints = sorted(Path(checkpoints_dir).glob("lm_head.*.pth"))
+    checkpoints = sorted(Path(checkpoints_dir).glob("lm_head.*.pth"))
+    natsorted(checkpoints)
     #checkpoints = checkpoints.sort(key=natural_keys)
-    checkpoints = sorted(Path(checkpoints_dir).glob("lm_head.*.pth"), key=lambda f: [int(n) for n in re.findall(r"\d+", f)])
+    #checkpoints = sorted(Path(checkpoints_dir).glob("lm_head.*.pth"), key=lambda f: [int(n) for n in re.findall(r"\d+", f)])
     print('zzzzzzzzzzz', checkpoints)
     assert len(checkpoints) > 0, f"no checkpoint files found in {checkpoints_dir}"
 
