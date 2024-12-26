13d655a493842524bf48a53ace47d44d7095f337
ychen514 <ychen514@uic.edu>
2024-12-26 04:47:56 -0800

    update

--- early_exit.py
+++ early_exit.py
@@ -301,7 +301,7 @@ def early_exit_lm_head(lm_models, out, lm_head):
 
     early_rate = 0.88
     if lm_head == 8:
-        early_rate = 0.886484375
+        early_rate = 0.8916015625
     elif lm_head == 6:
         early_rate = 0.886484375
     elif lm_head == 4:
