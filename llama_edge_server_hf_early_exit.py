0fb3d4e465920d0d89761cf94f5140f5d86ecb5f
ychen514 <ychen514@uic.edu>
2025-01-18 18:17:12 -0800

    update

--- llama_edge_server_hf_early_exit.py
+++ llama_edge_server_hf_early_exit.py
@@ -483,7 +483,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, early_idx_buff, end
 
         if out is None:
             http_receiver.set_outgoing_queue([-1, None, None])
-            max_layers = start_idx - 1 + max_layer_amount
+            max_layers = start_idx - 3 + max_layer_amount
             models, end_idx_buff = layer_reallocation(3, start_idx, end_idx_buff, max_layers, models)
             lm_head, _ = get_lm_head_idx(end_idx)
             if not lm_head == head_idx:
