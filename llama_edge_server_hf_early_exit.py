9a911c7c5a5054a13d37a9ce8e45aa6416650ede
ychen514 <ychen514@uic.edu>
2025-01-18 17:57:35 -0800

    update

--- llama_edge_server_hf_early_exit.py
+++ llama_edge_server_hf_early_exit.py
@@ -488,7 +488,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, early_idx_buff, end
             lm_head, _ = get_lm_head_idx(end_idx)
             if not lm_head == head_idx:
                 head_idx, lm_models = load_lm_head(args.ckpt_dir_hf_sep, end_idx, device, cache_dir="llm_weights")
-            start_idx_buff = max(0, start_idx - 1)
+            start_idx_buff = max(0, start_idx - 3)
             end_idx = start_idx + opt_layer_amount
             layer_amount = opt_layer_amount
             #http_receiver.set_outgoing_queue([-1, None, None])
