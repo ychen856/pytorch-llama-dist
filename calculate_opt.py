d03e4aa9535e250cf9170b322812eb6e483c7eb5
ychen514 <ychen514@uic.edu>
2025-01-22 20:16:54 -0800

    update

--- calculate_opt.py
+++ calculate_opt.py
@@ -303,7 +303,7 @@ class Calcualte_opt(object):
             print('gateway sub list: ', gateway_sub_list)
             gateway_sub_list_temp = sorted(gateway_sub_list, key=lambda x: x[1])
             print('gateway sub list temp: ', gateway_sub_list_temp)
-            gateway_end_idx = gateway_sub_list_temp[i][1]
+            gateway_end_idx = gateway_comp_time_temp[i][1]
             for j in range(0, len(gateway_sub_list_temp)):
                 if gateway_end_idx == gateway_sub_list_temp[j][1]:
                     print('clientPPPPPP: ', gateway_sub_list_temp[j])
