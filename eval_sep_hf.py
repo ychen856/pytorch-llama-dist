7d684c12bcfc258b7261084918518ca421e36495
ychen514 <ychen514@uic.edu>
2025-02-13 09:09:14 -0800

    update

--- eval_sep_hf.py
+++ eval_sep_hf.py
@@ -18,7 +18,7 @@ def get_eval_data(tokenizer):
     #random.seed(datetime.now().timestamp())
     # Get the test loader
     _, testloader = get_loaders(
-        dataset, seed=1, seqlen=seqlen, tokenizer=tokenizer
+        dataset, seed=0, seqlen=seqlen, tokenizer=tokenizer
         #dataset, seed=random.random(), seqlen=seqlen, tokenizer=tokenizer
     )
 
