7d6c1493e2301587c07de08cf61262af66e44acc
ychen514 <ychen514@uic.edu>
2025-02-13 20:42:03 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1.py
+++ llama_edge_hf_early_exit_jetson2_d1.py
@@ -444,7 +444,9 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
 
                 #print('updated end idx: ', end_idx)
                 break
-
+        print('out size: ', out.shape)
+        print('mask size: ', mask.shape)
+        print('fffff: ', mask)
         end_time = time.time()
         #print('client computation time: ', end_time - start_time)
 
