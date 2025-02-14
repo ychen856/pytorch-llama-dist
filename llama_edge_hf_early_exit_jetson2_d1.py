7423645e4268d222064af81145fb01baf1426f33
ychen514 <ychen514@uic.edu>
2025-02-13 22:42:50 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1.py
+++ llama_edge_hf_early_exit_jetson2_d1.py
@@ -448,6 +448,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
         print('mask: ', mask)
         print('out shape: ', out.last_hidden_state.shape)
         print('mask shape: ', mask.shape)
+        get_pruning_rate(out.last_hidden_state)
         #print('outlier shape: ', get_outlier(out.last_hidden_state).shape)
         end_time = time.time()
         #print('client computation time: ', end_time - start_time)
