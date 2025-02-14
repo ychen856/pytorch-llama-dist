507c372af447f8b67a4997944738bc8ab390fc2a
ychen514 <ychen514@uic.edu>
2025-02-13 22:01:22 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1.py
+++ llama_edge_hf_early_exit_jetson2_d1.py
@@ -450,7 +450,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
         print('mask shape: ', mask.shape)
         print('out flatten: ', out.last_hidden_state.flatten(start_dim=1).shape)
         for flatten_tensor in out.last_hidden_state.flatten(start_dim=1):
-            print('outlier: ', get_outlier(flatten_tensor.shape))
+            print('outlier: ', get_outlier(flatten_tensor))
         #print('outlier shape: ', get_outlier(out.last_hidden_state).shape)
         end_time = time.time()
         #print('client computation time: ', end_time - start_time)
