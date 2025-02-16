dfab4007833d53167ac220d11db90a7f73d78c5f
ychen514 <ychen514@uic.edu>
2025-02-15 22:54:53 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1.py
+++ llama_edge_hf_early_exit_jetson2_d1.py
@@ -480,12 +480,12 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
 
             print('out: ', out)
             outlier = get_outlier(out.last_hidden_state.flatten())
-
+            '''
             if outlier > 0:
                 rate = 50 / outlier
             else:
                 rate = 0.5
-            '''
+            
             pruned_feature_vector = prune_feature_vector(out.last_hidden_state, rate)
             csr_out = dense_to_CSR(pruned_feature_vector[0])'''
 
