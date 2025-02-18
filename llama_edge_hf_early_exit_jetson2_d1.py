742bd61d8c2bc753564a51953bd241bb97ddd73a
ychen514 <ychen514@uic.edu>
2025-02-17 18:57:11 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1.py
+++ llama_edge_hf_early_exit_jetson2_d1.py
@@ -492,6 +492,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
             pruned_feature_vector = prune_feature_vector(out.last_hidden_state, mean, rate)
             #csr_out = dense_to_CSR(pruned_feature_vector[0])
             csc_out = dense_to_CSC(pruned_feature_vector[0])
+            print('ZZZZZZZZZZ: ', len(csc_out))
 
             #out2 = csc_to_dense(csc_out)
             #print('out2 size: ', out2.shape)
