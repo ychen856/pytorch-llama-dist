d9890ea3b7ae3e48aead6ff77e20d64ff215bd7c
ychen514 <ychen514@uic.edu>
2025-02-24 10:21:22 -0800

    update

--- llama_edge_server_hf_early_exit.py
+++ llama_edge_server_hf_early_exit.py
@@ -525,7 +525,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, early_idx_buff, end
             layer_amount = opt_layer_amount
             #http_receiver.set_outgoing_queue([-1, None, None])
             continue
-        elif len(csr_out[0]) > 0:
+        elif csr_out[0] is not None :
             #recover from csr/csc
             out = BaseModelOutputWithPast()
             out.last_hidden_state = csr_to_dense(csr_out).unsqueeze(0)
