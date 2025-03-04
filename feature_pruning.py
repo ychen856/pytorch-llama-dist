9b7efd2ee1513cd23657a3d90a775e3c4a8db404
ychen514 <ychen514@uic.edu>
2025-03-04 11:23:48 -0800

    update

--- feature_pruning.py
+++ feature_pruning.py
@@ -28,7 +28,7 @@ def get_outlier2(flat_tensor):
 
 def get_outlier(flat_tensor, M):
     mean = flat_tensor.double().mean()
-    outliers = np.sum(np.abs(flat_tensor - mean) > M * mean)
+    outliers = np.sum(torch.abs(flat_tensor - mean) > M * mean)
 
     return mean,outliers
 
