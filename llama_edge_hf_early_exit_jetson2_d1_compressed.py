1f71807299861f1882f385589b72d80af1be4c1e
ychen514 <ychen514@uic.edu>
2025-03-24 12:39:31 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1_compressed.py
+++ llama_edge_hf_early_exit_jetson2_d1_compressed.py
@@ -310,12 +310,12 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
     count = 0
     early_count = 0
     statistics_period = calculate_opt.statistic_period
-    batch_size = 10
+    batch_size = 5
     # repeated 5->0, 10->1, 20->3
     global repeated
     #while not input_queue.empty():
     while(1):
-        if input_queue.qsize() == 0 and repeated == 1:
+        if input_queue.qsize() == 0 and repeated == 0:
             #time.sleep(150)
             while len(timestamp_manager.end_times) < batch_size:
                 time.sleep(0.0001)
@@ -369,7 +369,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
             batch_count = batch_count - 1
             repeated = 0
 
-        if repeated < 1:
+        if repeated < 0:
             for data in temp:
                 # print('data: ', data)
                 input_queue.put(data)
@@ -402,7 +402,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
         for k in range(1, end_idx + 1):
             try:
                 out, ids, mask = models[k](out.last_hidden_state, position_ids=ids, attention_mask=mask)
-                '''if k == head_idx:
+                if k == head_idx:
                     try:
                         is_early_exit, lm_logits = early_exit_lm_head(lm_models, out, head_idx)
                         #print('is early: ', is_early_exit)
@@ -416,7 +416,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
                     if is_early_exit:
                         print('end idx: ', k)
                         timestamp_manager.end_times = (idx, time.time())
-                        break'''
+                        break
 
             except Exception as e:
                 print('oom!!!')
