aee20cb73b4631a80a03dcf60b8540c1fb41ddda
ychen514 <ychen514@uic.edu>
2025-03-25 14:27:07 -0800

    update

--- llama_edge_server_hf_early_exit_prune.py
+++ llama_edge_server_hf_early_exit_prune.py
@@ -633,7 +633,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, early_idx_buff, end
 
         if not is_early_exit and end_idx < 34 and start_idx != 0:
             #prune feature vectur
-            mean, outlier = get_outlier(out.last_hidden_state, 3)
+            mean, outlier = get_outlier(out.last_hidden_state, 5)
             print('#outlier: ', outlier)
             rate = 10 / (10 * math.log10(outlier + 10))
 
