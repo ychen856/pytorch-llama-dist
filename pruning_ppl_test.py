e3bd2703a36a0febd3c9442eed1f688eecf562ad
ychen514 <ychen514@uic.edu>
2025-04-03 06:45:39 -0800

    update

--- pruning_ppl_test.py
+++ pruning_ppl_test.py
@@ -337,7 +337,7 @@ if __name__ == '__main__':
                     #print('splitting point: ', splitting_point)
                     #print('k: ', k)
                     if k == splitting_point:
-                        mean, outlier = get_outlier(out.last_hidden_state, 110)
+                        mean, outlier = get_outlier(out.last_hidden_state, 90)
                         print('#outlier: ', outlier)
                         rate = 10 / (10 * math.log10(outlier + 10))
                         print('rate: ', rate)
