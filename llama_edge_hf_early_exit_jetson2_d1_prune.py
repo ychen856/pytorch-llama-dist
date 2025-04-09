7044441598baea8cbb32932cb8efe8ca7f19bf2b
ychen514 <ychen514@uic.edu>
2025-04-09 13:11:10 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1_prune.py
+++ llama_edge_hf_early_exit_jetson2_d1_prune.py
@@ -475,7 +475,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
             #outlier calculation method II
             mean, outlier = get_outlier(out.last_hidden_state, 2000)
             print('#outlier: ', outlier)
-            rate = 10/(10 * math.log10(outlier + 10))
+            rate = 10/(10 * math.log10(outlier + 10) + 1.11)
 
             print('rate: ', rate)
             pruned_feature_vector = prune_feature_vector(out.last_hidden_state, mean, rate)
