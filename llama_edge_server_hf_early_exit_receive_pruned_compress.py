87c926f76958b424196d582f90c1b5ea3151e63d
ychen514 <ychen514@uic.edu>
2025-04-10 01:26:45 -0800

    update

--- llama_edge_server_hf_early_exit_receive_pruned_compress.py
+++ llama_edge_server_hf_early_exit_receive_pruned_compress.py
@@ -467,7 +467,7 @@ def task1_data_sending(args):
                     out.last_hidden_state = csr_out
                     mean, outlier = get_outlier(out.last_hidden_state, 2000)
                     print('#outlier: ', outlier)
-                    rate = 10 / (10 * math.log10(outlier + 10))
+                    rate = 10 / (10 * math.log10(outlier + 10) + 1.11)
 
                     print('rate: ', rate)
                     pruned_feature_vector = prune_feature_vector(out.last_hidden_state, mean, rate)
@@ -649,7 +649,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, early_idx_buff, end
             # prune and comress the feature vector
             mean, outlier = get_outlier(out.last_hidden_state, 2000)
             print('#outlier: ', outlier)
-            rate = 10 / (10 * math.log10(outlier + 10))
+            rate = 10 / (10 * math.log10(outlier + 10) + 1.11)
 
             print('rate: ', rate)
             pruned_feature_vector = prune_feature_vector(out.last_hidden_state, mean, rate)
