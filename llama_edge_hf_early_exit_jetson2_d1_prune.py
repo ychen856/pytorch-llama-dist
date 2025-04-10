e8d4606dec790c005f60d2b32a0cde3edcbe2ba5
ychen514 <ychen514@uic.edu>
2025-04-10 01:04:42 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1_prune.py
+++ llama_edge_hf_early_exit_jetson2_d1_prune.py
@@ -404,7 +404,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
         for k in range(1, end_idx + 1):
             try:
                 out, ids, mask = models[k](out.last_hidden_state, position_ids=ids, attention_mask=mask)
-                '''if k == head_idx:
+                if k == head_idx:
                     try:
                         is_early_exit, lm_logits = early_exit_lm_head(lm_models, out, head_idx)
                         #print('is early: ', is_early_exit)
@@ -418,7 +418,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
                     if is_early_exit:
                         print('end idx: ', k)
                         timestamp_manager.end_times = (idx, time.time())
-                        break'''
+                        break
 
             except Exception as e:
                 print('oom!!!')
