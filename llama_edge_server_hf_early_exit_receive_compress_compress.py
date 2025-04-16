e3b60d8dc9eef9b905d83854295518a599de77e2
ychen514 <ychen514@uic.edu>
2025-04-15 21:43:46 -0800

    update

--- llama_edge_server_hf_early_exit_receive_compress_compress.py
+++ llama_edge_server_hf_early_exit_receive_compress_compress.py
@@ -479,7 +479,7 @@ def task1_data_sending(args):
                 else:
                     mean, outlier = get_outlier(out.last_hidden_state, 2000)
                     print('#outlier: ', outlier)
-                    rate = 10 / (10 * math.log10(outlier + 10))
+                    rate = 10 / (10 * math.log10(outlier + 10) + 1.11)
 
                     print('rate: ', rate)
                     pruned_feature_vector = prune_feature_vector(out.last_hidden_state, mean, rate)
@@ -583,7 +583,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, early_idx_buff, end
             else:
                 mean, outlier = get_outlier(out.last_hidden_state, 2000)
                 print('#outlier: ', outlier)
-                rate = 10 / (10 * math.log10(outlier + 10))
+                rate = 10 / (10 * math.log10(outlier + 10) + 1.11)
 
                 print('rate: ', rate)
                 pruned_feature_vector = prune_feature_vector(out.last_hidden_state, mean, rate)
@@ -669,7 +669,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, early_idx_buff, end
             # prune and comress the feature vector
             mean, outlier = get_outlier(out.last_hidden_state, 2000)
             print('#outlier: ', outlier)
-            rate = 10 / (10 * math.log10(outlier + 10))
+            rate = 10 / (10 * math.log10(outlier + 10) + 1.11)
 
             print('rate: ', rate)
             pruned_feature_vector = prune_feature_vector(out.last_hidden_state, mean, rate)
