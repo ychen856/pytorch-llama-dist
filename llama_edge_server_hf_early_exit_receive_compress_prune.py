7f508957e5b7fc1b35364f9561851d51f3232e75
ychen514 <ychen514@uic.edu>
2025-05-16 05:04:06 -0800

    update

--- llama_edge_server_hf_early_exit_receive_compress_prune.py
+++ llama_edge_server_hf_early_exit_receive_compress_prune.py
@@ -158,6 +158,7 @@ def layer_reallocation(type, start_idx, end_idx_buff, max_layers, models):
         print('end idx buff: ', end_idx_buff)
         for checkpoint in checkpoints:
             print('checkpoint idx: ', checkpoint_idx)
+            print('models length: ', len(models))
             if checkpoint_idx > len(models):
                 ckpt_path = checkpoint
                 checkpoint_list.append(torch.load(ckpt_path, map_location="cpu"))
