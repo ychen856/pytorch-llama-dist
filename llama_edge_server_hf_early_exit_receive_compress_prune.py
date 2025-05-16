28f6638861f72ae92cf79d6b6621cfd98bedba8a
ychen514 <ychen514@uic.edu>
2025-05-16 04:25:08 -0800

    update

--- llama_edge_server_hf_early_exit_receive_compress_prune.py
+++ llama_edge_server_hf_early_exit_receive_compress_prune.py
@@ -138,7 +138,7 @@ def layer_reallocation(type, start_idx, end_idx_buff, max_layers, models):
         #checkpoints = checkpoints[start_idx_buff:max_layers]
 
 
-        checkpoints = checkpoints[start_idx_buff:max_layers]
+        checkpoints = checkpoints[start_idx_buff:max_layers + 1]
         print('checkpoints: ', checkpoints)
         print('model: ', models)
         print('YACACA')
