4cf2ff82fc4864090b811d7ab0a41ce9dc29b773
ychen514 <ychen514@uic.edu>
2025-05-29 08:43:25 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1.py
+++ llama_edge_hf_early_exit_jetson2_d1.py
@@ -287,7 +287,7 @@ def task1_data_sending(args):
         data = outgoing_queue.get()
         performance_data_store.outgoing_count = performance_data_store.outgoing_count + 1
         #http_sender.send_data(args.server_ip, args.server_port, data, calculate_opt, timestamp_manager)
-        http_sender.send_data(args.server_ip, args.server_port, data, performance_data_store, timestamp_manager)
+        http_sender.send_data(args.gateway_ip, args.gateway_port, data, performance_data_store, timestamp_manager)
 
 
 def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_idx, max_layers, device):
