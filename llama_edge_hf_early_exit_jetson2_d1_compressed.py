b3cbc6dc30459f34eea3855562706b1989060bb9
ychen514 <ychen514@uic.edu>
2025-05-30 01:17:37 -0800

    update

--- llama_edge_hf_early_exit_jetson2_d1_compressed.py
+++ llama_edge_hf_early_exit_jetson2_d1_compressed.py
@@ -310,12 +310,12 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
     count = 0
     early_count = 0
     statistics_period = calculate_opt.statistic_period
-    batch_size = 10
+    batch_size = 5
     # repeated 5->0, 10->1, 20->3
     global repeated
     #while not input_queue.empty():
     while(1):
-        if input_queue.qsize() == 0 and repeated == 1:
+        if input_queue.qsize() == 0 and repeated == 0:
             #time.sleep(150)
             while len(timestamp_manager.end_times) < batch_size:
                 time.sleep(0.0001)
@@ -369,7 +369,7 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
             batch_count = batch_count - 1
             repeated = 0
 
-        if repeated < 1:
+        if repeated < 0:
             for data in temp:
                 # print('data: ', data)
                 input_queue.put(data)
@@ -601,8 +601,8 @@ if __name__ == '__main__':
         #input_queue.put(inputs)
         temp.append(inputs)
 
-    random.seed(datetime.now().timestamp())
-    random.shuffle(temp)
+    #random.seed(datetime.now().timestamp())
+    #random.shuffle(temp)
     temp = temp[:5]
 
     for i in range(0, batch_size):
