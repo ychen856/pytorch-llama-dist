c54532c6565e265348ce9b9636f9b0084215f2aa
ychen514 <ychen514@uic.edu>
2025-06-11 08:22:38 -0800

    update

--- detokenizer_finetuning.py
+++ detokenizer_finetuning.py
@@ -278,7 +278,7 @@ if __name__ == '__main__':
                 # Prepare inputs and move to device
                 #inputs = testenc[:, (i * seqlen):(j * seqlen)].to(device)
                 #inputs = inputs.reshape(j - i, seqlen)
-                inputs = trainenc[i]
+                inputs = trainenc[i].to(device)
                 decoded_data = None
                 #print('inputs: ', inputs)
                 #print('inputs size: ', inputs.shape)
