8840a46aa5331548f19dec0b5036cc1fb99b7122
ychen514 <ychen514@uic.edu>
2025-06-11 06:00:56 -0800

    update

--- detokenizer_finetuning.py
+++ detokenizer_finetuning.py
@@ -7,7 +7,7 @@ import torch
 import time
 from pathlib import Path
 import argparse
-
+'''
 from natsort import natsorted
 import torch.nn as nn
 from transformers import PreTrainedTokenizerFast, LlamaTokenizer, AutoModelForCausalLM, LlamaConfig, AutoConfig
@@ -18,10 +18,10 @@ import sys
 
 from early_exit import early_exit_lm_head
 from eval_sep_hf import get_eval_data, get_train_data
-'''from model_hf import LlamaForCausalLM, LlamaForCausalLM_emb, LlamaForCausalLM_layer_0, LlamaForCausalLM_norm, \
-    LlamaForCausalLM_linear
+from model_hf import LlamaForCausalLM, LlamaForCausalLM_emb, LlamaForCausalLM_layer_0, LlamaForCausalLM_norm, \
+    LlamaForCausalLM_linear'''
 import yaml
-from feature_decoder import *
+'''from feature_decoder import *
 from torch.cuda.amp import GradScaler, autocast'''
 
 parser = argparse.ArgumentParser(
