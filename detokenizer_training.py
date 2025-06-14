89e1e68d180a06071c9a0d576db8062f8f22835b
ychen514 <ychen514@uic.edu>
2025-06-13 17:26:31 -0800

    update

--- detokenizer_training.py
+++ detokenizer_training.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
     possible_ks = [32, 64, 128],
     possible_bottleneck_dims = [64, 128, 256, 512]
 
-    models = load_model(args.ckpt_dir_hf_sep, 0, 34, device)
+    models = load_model(args.ckpt_dir_hf_sep, 0, 1, device)
     tokenizer = LlamaTokenizer.from_pretrained(args.ckpt_dir_hf, use_fast=False)
     encoder = FlexibleTopKEncoder().to(device)
     decoder = FlexibleDecoder(seq_len=seqlen).to(device)
@@ -276,7 +276,8 @@ if __name__ == '__main__':
             for i in range(0, len(trainenc), bs):
                 j = min(i + bs, nsamples)
                 optimizer.zero_grad(set_to_none=True)
-                topk = random.choice(possible_ks)
+                top_k = random.choice(possible_ks)
+                print('kkkkkkkkkkkkk: ', top_k)
                 bottleneck_dim = random.choice(possible_bottleneck_dims)
 
                 inputs = trainenc[i].to(device)
@@ -291,7 +292,7 @@ if __name__ == '__main__':
                             # Step 1: compute confidence + top-k
                             probs = torch.softmax(lm_logits, dim=-1)
                             conf = probs.max(dim=-1).values  # [B, 1024]
-                            topk_vals, topk_idx = conf.topk(topk, dim=1)
+                            topk_vals, topk_idx = conf.topk(top_k, dim=1)
                             B, _, V = lm_logits.shape
                             topk_idx_exp = topk_idx.unsqueeze(-1).expand(-1, -1, V)
                             topk_logits = torch.gather(lm_logits, dim=1, index=topk_idx_exp)
