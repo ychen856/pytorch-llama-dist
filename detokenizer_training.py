e245ac859cb6b8023bebf04bad7c13f900d6131c
ychen514 <ychen514@uic.edu>
2025-06-13 17:38:21 -0800

    update

--- detokenizer_training.py
+++ detokenizer_training.py
@@ -231,7 +231,7 @@ if __name__ == '__main__':
     seqlen = 128
     bs = 1
 
-    possible_ks = [32, 64, 128],
+    possible_ks = [32, 64, 128]
     possible_bottleneck_dims = [64, 128, 256, 512]
 
     models = load_model(args.ckpt_dir_hf_sep, 0, 1, device)
