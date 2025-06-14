a827cdece2688f441bc70b3a8ec4f4113dddb5cd
ychen514 <ychen514@uic.edu>
2025-06-14 01:30:14 -0800

    update

--- detokenizer_training.py
+++ detokenizer_training.py
@@ -233,7 +233,8 @@ if __name__ == '__main__':
     seqlen = 1024
     bs = 1
 
-    possible_ks = [32, 64, 128]
+    #possible_ks = [32, 64, 128]
+    possible_ks = [64]
     possible_bottleneck_dims = [64, 128, 256, 512]
 
     models = load_model(args.ckpt_dir_hf_sep, 0, 34, device)
@@ -283,7 +284,7 @@ if __name__ == '__main__':
                 else:
                     top_k = args.k
 
-                top_k = 64
+                #top_k = 64
                 print('top k: ', top_k)
                 bottleneck_dim = random.choice(possible_bottleneck_dims)
 
