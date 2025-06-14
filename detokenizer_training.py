3938eb4893cb08b1a4f2c8b69c2dcf3b685e348f
ychen514 <ychen514@uic.edu>
2025-06-14 15:11:06 -0800

    update

--- detokenizer_training.py
+++ detokenizer_training.py
@@ -251,7 +251,7 @@ if __name__ == '__main__':
     scaler = GradScaler()
     optimizer = AdamW(
         list(encoder.parameters()) + list(decoder.parameters()),
-        lr=1e-6,
+        lr=1e-5,
         weight_decay=0.01
     )
     opt_ppl = float('inf')
